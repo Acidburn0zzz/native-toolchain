84ae72594dee2ed12931cd608c6edf62e6960032
Valencia Serrao <vserrao@us.ibm.com>
2016-12-08 13:47:38 +0000

    init.sh updated with ppc-specific arch flags

--- init.sh
+++ init.sh
@@ -134,7 +134,7 @@ export COMPILER_VERSION
 
 # ARCH_FLAGS are used to convey architectur dependent flags that should
 # be obbeyed by libraries explicitly needing this information.
-ARCH_FLAGS="-mno-avx2"
+ARCH_FLAGS="-mvsx -maltivec"
 
 # Check Platform and build the correct release name. The RELEASE_NAME is used
 # when publishing the artifacts to the artifactory.
